a75e8c7c261032079b283307494d1440b3187b5c
miguelryan-1305 <miguel.diniz.araujo@escola.pr.gov.br>
2026-09-15 11:21:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Animação simples nos cards
 
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+    card.addEventListener("mouseenter", () => {
+        card.style.transform = "translateY(-5px)";
+    });
+
+    card.addEventListener("mouseleave", () => {
+        card.style.transform = "translateY(0)";
+    });
+
+});
+
+
+// Mensagem no console
+
+console.log("Portfólio de Miguel Ryan carregado!");
